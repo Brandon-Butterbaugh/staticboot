cc576bc23f67e6ce383cabb53304f724889bf4e6
Brandon-Butterbaugh <bjbutterbaugh@comcast.net>
2025-10-16 22:04:51 -0500

    made static and code to copy to public

--- src/main.py
+++ src/main.py
@@ -1,8 +1,32 @@
+import os
+import shutil
+
 from textnode import TextNode, TextType
 
 
 
 def main():
-    node = TextNode("This is some anchor text", TextType.LINKS, "https://www.boot.dev")
-    print(node)
+    if os.path.exists("./public"):
+        shutil.rmtree("./public")
+    copy_files_recursive("./static", "./public")
+
+
+
+
+
+
+def copy_files_recursive(source_dir_path, dest_dir_path):
+    if not os.path.exists(dest_dir_path):
+        os.mkdir(dest_dir_path)
+
+    for filename in os.listdir(source_dir_path):
+        from_path = os.path.join(source_dir_path, filename)
+        dest_path = os.path.join(dest_dir_path, filename)
+        print(f" * {from_path} -> {dest_path}")
+        if os.path.isfile(from_path):
+            shutil.copy(from_path, dest_path)
+        else:
+            copy_files_recursive(from_path, dest_path)
+
+
 main()
